f17d413a89d1cf623804895786e7d028fc6d1b39
张小生 <zhangxiaosheng@zhangxiaoshengdeMacBook-Air.local>
2023-08-09 20:54:31 +0800

    init

---
+++ cli.js
@@ -0,0 +1,7 @@
+#! /usr/local/bin/node
+
+// process.env.UMI_PLUGINS = require.resolve('./plugin')
+const umiBinPath = require.resolve('umi/bin/umi');
+require('child_process').fork(umiBinPath, process.argv.slice(2), {
+    stdio: 'inherit'
+})
---
+++ plugin.js
@@ -0,0 +1,17 @@
+import {type as IApi ,api } from 'umi';
+ 
+export default () => {
+  api.describe({
+    key: 'changeFavicon',
+    config: {
+      schema(joi) {
+        return joi.string();
+      },
+    },
+    enableBy: api.EnableBy.config
+  });
+  api.modifyConfig((memo)=>{
+    memo.favicons = api.userConfig.changeFavicon;
+    return memo;
+  });
+};
